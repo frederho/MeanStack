994c834a9ce7d0d931ac9920f2aabe5670eeae27
frederho <frederikkhols@gmail.com>
2015-01-20 15:13:23 +0100

    added ui-router

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-angular.module('flapperNews', ['flapperNews.controllers','flapperNews.services', 'ui.router']);
+angular.module('flapperNews', ['flapperNews.controllers','flapperNews.services', 'ui.router'])
+.config([
+		'$stateProvider',
+		'$urlRouterProvider',
+		function($stateProvider, $urlRouterProvider) {
+			$stateProvider
+				.state('home', {
+					url: '/home',
+					templateUrl: 'partials/home.html',
+					controller: 'MainCtrl',
+					controllerAs: 'vm'
+				});
+			$urlRouterProvider.otherwise('home');
+		}
+	]);
+	
