42ea111dd7af468d6e487e771c9aab43e6b201fd
frederho <frederikkhols@gmail.com>
2015-01-22 12:15:34 +0100

    Revert "Revert "Finish up comments part""
    This reverts commit cecf39a640f86bf4ff0b32a27f9a7562a417e555.

--- flapper-news/public/javascript/controllers/PostsCtrl.js
+++ flapper-news/public/javascript/controllers/PostsCtrl.js
@@ -32,5 +32,21 @@ angular.module('flapperNews.controllers',[])
 	'$stateParams',
 	'postsHandler',	function($stateParams, postsHandler){
 		var vm = this;
+		vm.body = '';
 		vm.post = postsHandler.getPost($stateParams.id);
+
+		vm.upvoteComment = function (comment){
+			comment.upvotes += 1;
+		};
+
+		vm.addComment = function () {
+			var newComment = {
+				body: vm.body,
+				author: 'user',
+				upvotes: 0
+			};
+			vm.post.comments.push(newComment);
+			vm.body = '';
+
+		};
 	}]);
